a68cec95dc10024421b242c22046cf0d24f217da
U-OK <thelavallet@gmail.com>
2020-07-21 17:18:22 +0700

    Выполнил первое задание

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Test from "./Test";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/test">Test</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/test">
+            <Test />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h1>I'm home</h1>;
 }
 
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { connect } from "react-redux";
+import { increment, decrement } from "./redux/actions";
+
+import { Button } from "@material-ui/core";
+
+class Test extends React.Component {
+  _plus = () => {
+    this.props.increment();
+  };
+
+  _minus = () => {
+    this.props.decrement();
+  };
+
+  render() {
+    const { count } = this.props;
+    return (
+      <div>
+        <h1>I'm test</h1>
+        <h2>Count: {count}</h2>
+        <div>
+          <Button onClick={this._plus} variant="contained" color="primary">
+            +
+          </Button>
+          <Button onClick={this._minus} variant="contained" color="primary">
+            -
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  count: state.testReducer.count,
+});
+
+const mapDispatchToProps = {
+  increment,
+  decrement,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Test);
